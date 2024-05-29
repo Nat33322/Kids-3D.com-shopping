e9536f21f17bcbd69acafccef46ae7c68a3bf13a
Nat <165587088+Nat33322@users.noreply.github.com>
2024-05-28 19:29:01 -0700

    Update emailhandler.php

--- emailhandler.php
+++ emailhandler.php
@@ -1,6 +1,14 @@
-<script>
 <?php
-mail("njflock11@gmail.com", "Website quiz upload",
-$_POST["correct"]. "From: njflock11@gmail.com");
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $message = $_POST['message'];
+  $to = 'recipient@example.com';
+  $subject = 'New message from your website';
+  
+  $headers = "From: " . $email;
+  
+  mail($to, $subject, $message, $headers);
+  echo "Email sent!";
+}
 ?>
-</script>
