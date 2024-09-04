7763bf4627b4cd380f38b112066748292984b39b
Nat <165587088+Nat33322@users.noreply.github.com>
2024-09-04 15:45:08 -0700

    Update datastore.py

--- .gitignore/datastore.py
+++ .gitignore/datastore.py
@@ -1,1 +1,16 @@
+from datetime import datetime
+
+now = datetime.now()
+
+current_time = now.strftime("%D:%H:%M:%S")
+print("Current Time =", current_time)
+
+message = "(name) wants (product. requested at {current_time}"
+
+with open("datastore.txt","w") as f: #in write mode
+    f.write("{name} wants {product}.".format(list))
+
+with open("datastore.txt") as f: #in read mode, not in write mode, careful
+    rd=f.readlines()
+print (rd)
 
